21957f23e40fdf249c1909dffde0830fa3dcdc80
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-07-10 17:07:26 -0400

    updating prd from stg

---
+++ DB_Files/DSS_3_PRODUCTION_DB/DEMO/TABLES/DATAGRIP_TABLE.sql
@@ -0,0 +1,5 @@
+create or replace TABLE DATAGRIP_TABLE (
+	COLUMN_2 NUMBER(38,0) COMMENT 'and one more',
+	BUILDING_CODE VARCHAR(16777216) COMMENT 'it builds the code on this mess as I go!'
+)COMMENT='oh I can add a comment'
+;
