ec42677fa389b8642e3a18df366e4da1318791db
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-07-17 15:49:47 -0400

    syncing up stg and prd for more testing

---
+++ Schema_Change_Drops/PRD_deploy/V1.1.2__testing.sql
@@ -0,0 +1,9 @@
+alter table DEMO.FOGGY_TABLE
+    add NICKNAME NUMBER;
+
+create view demo.addresses_to_delete_v as select * from DEMO.addresses;
+
+create view demo.addresses_v as select * from DEMO.addresses;
+
+create view demo.hot_chicken_v as select * from DEMO.addresses;
+
