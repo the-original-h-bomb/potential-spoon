c224c71e874157e59e6ebca0b655f5c7b145d0aa
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-08-14 14:02:01 -0400

    New Table creation

---
+++ Schema_Change_Drops/Jen_Redux_stg_deploy/V2023.08.2__butter_bomb_2.sql
@@ -0,0 +1,16 @@
+create table HEATHER.BUTTER_BOMB
+(
+    CATALOG_NAME                  VARCHAR,
+    SCHEMA_NAME                   VARCHAR,
+    SCHEMA_OWNER                  VARCHAR,
+    IS_TRANSIENT                  VARCHAR(3),
+    IS_MANAGED_ACCESS             VARCHAR(3),
+    RETENTION_TIME                NUMBER,
+    DEFAULT_CHARACTER_SET_CATALOG VARCHAR,
+    DEFAULT_CHARACTER_SET_SCHEMA  VARCHAR,
+    DEFAULT_CHARACTER_SET_NAME    VARCHAR,
+    SQL_PATH                      VARCHAR,
+    CREATED                       TIMESTAMPLTZ(3),
+    LAST_ALTERED                  TIMESTAMPLTZ(3),
+    COMMENT                       VARCHAR
+);
