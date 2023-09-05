59d944a3d4b1988cd87f577e84265872f4804fff
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-09-05 16:32:40 -0400

    Building out test sources in hdg dev/stg

---
+++ Schema_Change_Drops/FV70710_STG_deploy/V2023.3.3__correcting dynamic table.sql
@@ -0,0 +1,7 @@
+
+create or replace dynamic table ROLLBACK.FOOD_ID_DT(
+	ID,
+	FOOD
+) lag = '1 minute' warehouse = COMPUTE_WH
+ as
+select n.id, f.food from rollback.names n inner join rollback.foods f on n.code = f.code;
