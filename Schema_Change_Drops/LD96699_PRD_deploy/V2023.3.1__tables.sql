296a45d78550ad9fbd6ccf9c2041680415f1e290
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-09-12 16:08:04 -0400

    again and foods to prd

---
+++ Schema_Change_Drops/LD96699_PRD_deploy/V2023.3.1__tables.sql
@@ -0,0 +1,14 @@
+create table ROLLBACK.AND_AGAIN
+(
+    ID   NUMBER not null
+        primary key,
+    NAME VARCHAR(255),
+    CODE VARCHAR(255)
+);
+
+create table ROLLBACK.FOODS
+(
+    CODE VARCHAR(255),
+    FOOD VARCHAR(255)
+);
+
