a9543dff04774a7b1b46b332b26dd7c88de40e2b
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-09-25 15:14:13 -0400

    Update V2023.3.10__foods_restore.sql

--- Schema_Change_Drops/LD96699_STG_deploy/V2023.3.10__foods_restore.sql
+++ Schema_Change_Drops/LD96699_STG_deploy/V2023.3.10__foods_restore.sql
@@ -1,1 +1,1 @@
-create or replace table rollback.foods clone rollback.foods at(timestamp => '2023-09-25 11:54:33.964 -0700'::timestamp_tz); 
+create or replace table rollback.foods_1 clone rollback.foods at(timestamp => '2023-09-25 11:54:33.964 -0700'::timestamp_tz); 
