1de04cc148c9c941b0f62f86c8f830876c1ce3e5
the-original-h-bomb <127529152+the-original-h-bomb@users.noreply.github.com>
2023-09-25 14:07:43 -0400

    Create v2023.3.6__data.sql

---
+++ Schema_Change_Drops/LD96699_STG_deploy/v2023.3.6__data.sql
@@ -0,0 +1,20 @@
+Insert INTO rollback.foods values ('PIN','pineapple');
+Insert INTO rollback.foods values ('APL','apple');
+Insert INTO rollback.foods values ('CHR','cherry');
+Insert INTO rollback.foods values ('PIZ','pizza');
+Insert INTO rollback.foods values ('POT','potato');
+Insert INTO rollback.foods values ('CHL','chili');
+Insert INTO rollback.foods values ('HAM','ham');
+Insert INTO rollback.foods values ('CHK','chicken');
+Insert INTO rollback.foods values ('TOM','tomato');
+Insert INTO rollback.foods values ('QCH','quiche');
+Insert INTO rollback.foods values ('CHZ','cheese');
+
+Insert INTO rollback.names values ('93429','Perogi Pete','CHR');
+Insert INTO rollback.names values ('93430','Lucy Linguine','PIZ');
+Insert INTO rollback.names values ('93431','Tommy Tomato','HAM');
+Insert INTO rollback.names values ('93432','Priscilla Pistachio','CHK');
+Insert INTO rollback.names values ('93433','Candace Canned Canoli','APL');
+Insert INTO rollback.names values ('93434','Horace Hamburger','CHR');
+Insert INTO rollback.names values ('93435','Anthony Applesauce','PIN');
+
